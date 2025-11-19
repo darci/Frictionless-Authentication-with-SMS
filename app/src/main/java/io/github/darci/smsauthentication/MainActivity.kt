a7eca1d91799dac0abd2d430808d85d0bda1cca8
Darci Neto <darci.neto@hotmail.com>
2025-11-18 22:33:03 -0300

    first commit

---
+++ app/src/main/java/io/github/darci/smsauthentication/MainActivity.kt
@@ -0,0 +1,47 @@
+package io.github.darci.smsauthentication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.darci.smsauthentication.ui.theme.FrictionlessAuthenticationWithSMSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FrictionlessAuthenticationWithSMSTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FrictionlessAuthenticationWithSMSTheme {
+        Greeting("Android")
+    }
+}
