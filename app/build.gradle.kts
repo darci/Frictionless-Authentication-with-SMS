f25b46fb91136c93b39aacf38f18197dfd8a016b
Darci Neto <darci.neto@hotmail.com>
2026-03-13 17:01:48 -0300

    refactor build configuration to simplify compileSdk setup and add toolchain plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "io.github.darci.smsauthentication"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.darci.smsauthentication"
